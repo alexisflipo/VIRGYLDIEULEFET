9ae6d861002f8ff1863701c015cdcf3477acd2e4
Alexis Flipo <alexis.flipo1@gmail.com>
2021-02-03 12:45:42 +0100

    contact

--- app/javascript/components/zoom.js
+++ app/javascript/components/zoom.js
@@ -2,19 +2,20 @@ const zoom = () => {
   const buttons = document.querySelectorAll(".project");
   const overlay = document.querySelector(".overlay");
   const overlayImage = document.querySelector(".overlay__inner img");
+  if (overlay) {
+    function open(e) {
+      overlay.classList.add("open");
+      const src = e.currentTarget.querySelector("img").src;
+      overlayImage.src = src;
+    }
 
-  function open(e) {
-    overlay.classList.add("open");
-    const src = e.currentTarget.querySelector("img").src;
-    overlayImage.src = src;
+    function close() {
+      overlay.classList.remove("open");
+    }
+
+    buttons.forEach((button) => button.addEventListener("click", open));
+    overlay.addEventListener("click", close);
   }
-
-  function close() {
-    overlay.classList.remove("open");
-  }
-
-  buttons.forEach((button) => button.addEventListener("click", open));
-  overlay.addEventListener("click", close);
 };
 
 export { zoom };
