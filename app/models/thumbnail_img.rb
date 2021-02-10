095bca49ee8064d79bbbd1c53d098a7e698eaef1
Alexis Flipo <alexis.flipo1@gmail.com>
2021-02-10 06:26:30 +0100

    design responsive

--- app/models/thumbnail_img.rb
+++ app/models/thumbnail_img.rb
@@ -1,6 +1,6 @@
 class ThumbnailImg < ApplicationRecord
     has_one_attached :photo
-    validates :photo, size: { less_than: 11.megabytes, 
+    validates :photo, size: { less_than: 10.megabytes, 
     message: "La photo ne doit pas dépasser 10 mo"}
     validates :photo, attached: true
 end
