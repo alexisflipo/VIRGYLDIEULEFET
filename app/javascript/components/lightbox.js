b9c863015d9de9b967f76b33e9fb9a02f2b0921e
Alexis Flipo <alexis.flipo1@gmail.com>
2021-02-11 06:55:38 +0100

    responsive

--- app/javascript/components/lightbox.js
+++ app/javascript/components/lightbox.js
@@ -1,7 +1,6 @@
 const galery = () => {
   const imgs = document.querySelectorAll("#galery img");
   const overlay = document.querySelector("#parent");
-
   imgs.forEach((img) => {
     img.addEventListener("click", (e) => {
       e.preventDefault();
@@ -15,22 +14,43 @@ const galery = () => {
         </div>`;
 
       document.body.appendChild(dom);
-
+      document.addEventListener("click", (e) => {
+        if (overlay) {
+          const navbar = document.querySelector("nav");
+          const body = document.querySelector("body");
+          navbar.classList.add("disabled");
+          body.classList.add("scroll-disable");
+        }
+        // else {
+        //   const navbar = document.querySelector("nav");
+        //   const body = document.querySelector("body");
+        //   navbar.classList.remove("disabled");
+        //   body.classList.remove("scroll-disable");
+        // }
+      });
       const close = document.getElementById("child");
       const overlay = document.querySelector("#parent");
 
       if (close) {
         close.addEventListener("click", (e) => {
+          const body = document.querySelector("body");
+          const navbar = document.querySelector("nav");
           overlay.classList.add("fadeOut");
           window.setTimeout(() => {
+            navbar.classList.remove("disabled");
+            body.classList.remove("scroll-disable");
             close.parentNode.remove(overlay);
           }, 500);
         });
         window.addEventListener("keydown", (e) => {
           const escape = e.key;
+          const body = document.querySelector("body");
+          const navbar = document.querySelector("nav");
           if (escape === "Escape") {
             overlay.classList.add("fadeOut");
             window.setTimeout(() => {
+              navbar.classList.remove("disabled");
+              body.classList.remove("scroll-disable");
               close.parentNode.remove(overlay);
             }, 500);
           }
