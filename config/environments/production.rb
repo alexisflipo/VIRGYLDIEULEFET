cfe7ee4a64bbf20e55ce954c66999ffd37be5d81
Alexis Flipo <alexis.flipo1@gmail.com>
2021-03-01 22:25:42 +0100

    preload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  
+  config.action_view.preload_links_header = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
