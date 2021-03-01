07a8f57de012f749b1f70114c3fe9364a7d505d7
Alexis Flipo <alexis.flipo1@gmail.com>
2021-03-01 21:21:00 +0100

    js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   config.assets.compile = true
   config.assets.compress = true
   config.assets.css_compressor = :yui
-
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
